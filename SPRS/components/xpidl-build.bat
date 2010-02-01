7ac17e4f6b3189ef8602728280a640c585d9c5db
unknown <david@.(none)>
2010-01-31 16:20:15 -0800

    wrapper.idl "builds", batch file massaged

---
+++ SPRS/components/xpidl-build.bat
@@ -0,0 +1,4 @@
+#..\gecko-sdk\bin\xpidl.exe -m header -I..\gecko-sdk\idl %1
+#..\gecko-sdk\bin\xpidl.exe -m typelib -I..\gecko-sdk\idl %1
+..\..\..\gecko-sdk\bin\xpidl.exe -m header -I..\..\..\gecko-sdk\idl %1
+..\..\..\gecko-sdk\bin\xpidl.exe -m typelib -I..\..\..\gecko-sdk\idl %1
