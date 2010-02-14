b7aae46b7cc86620ad662fa9820c57761c866dc8
unknown <Dylan@.(none)>
2010-02-13 22:20:46 -0800

    Worked a bunch on utility.cpp. Not quite working yet

--- wrapper/src/utility.cpp
+++ wrapper/src/utility.cpp
@@ -4,8 +4,11 @@
 #include <windows.h>
 #include <string>
 #include <iostream>
+#include <fstream>
 
 #include "error.h"
+
+#define BUFFER_SIZE 256
 
 using namespace std;
 
@@ -13,9 +16,13 @@ HMODULE PKCSLibraryModule = 0;
 CK_RV	returnValue;
 CK_FUNCTION_LIST_PTR funcList;
 
+
 CK_SLOT_ID_PTR pSlotWithTokenList;
 CK_SESSION_HANDLE hSession;
 int TokenCount = 0;
+
+CK_BBOOL True = TRUE;		    
+CK_BBOOL False = FALSE;
 
 bool initCrypto() {
 	if(PKCSLibraryModule) {
@@ -80,7 +87,7 @@ string *enumerateCards(void)
 	while (1)
 		{
 			//Search readers and store result in pSlotWithTokenList;
-			returnValue = (funcList->C_GetSlotList)(CK_TRUE, pSlotWithTokenList, &ulSlotWithTokenCount);
+			returnValue = (funcList->C_GetSlotList)(TRUE, pSlotWithTokenList, &ulSlotWithTokenCount);
 
 			if (returnValue != CKR_BUFFER_TOO_SMALL)
 				break;
@@ -127,7 +134,120 @@ bool selectCard(int SlotID)
 			setError(OPEN_SESSION_FAILED);
 			return false;
 		}
+		returnValue = (funcList->C_Login)(hSession, CKU_USER, (CK_CHAR_PTR)"123", 3);
+		cout << returnValue << endl;
 }
+
+bool getPublicKey(string keyName, CK_OBJECT_HANDLE &pubKey);
+
+string encrypt( string plainText, string keyLabel )
+{
+	CK_ULONG encrypted = 128;
+	CK_OBJECT_HANDLE key;
+	CK_ULONG numKeysFound;
+	CK_ULONG plainTextLength;
+	CK_BYTE* inBuffer;
+	CK_BYTE* outBuffer; 
+	CK_MECHANISM mechanism = {CKM_RSA_PKCS, NULL_PTR, 0}; /* encrypt using RSA PCKS */	
+	CK_UTF8CHAR* label = (CK_UTF8CHAR*)keyLabel.c_str();
+	CK_ATTRIBUTE searchKey[] = {CKA_LABEL, label, sizeof(label) - 1};
+	
+	/* search for label which matches our key */
+	getPublicKey(keyLabel, key);
+
+//	funcList->C_FindObjectsFinal(hSession);
+	/* begin encryption */
+	if((funcList->C_EncryptInit)(hSession, &mechanism, key) != CKR_OK) 
+	{
+		setError(COULD_NOT_INIT_DECRYPTION);
+		return "";
+	}
+	
+	plainTextLength = sizeof(plainText.c_str());
+	inBuffer = (CK_BYTE*)plainText.c_str();
+	//encrypt once to get the size
+	returnValue = (funcList->C_Encrypt)(hSession, inBuffer, plainTextLength, NULL_PTR, &encrypted);
+	if(returnValue != CKR_OK) 
+	{
+			setError(FAILED_TO_ENCRYPT);
+			return "";
+	}
+	outBuffer = (CK_BYTE*)malloc(sizeof(CK_BYTE) * encrypted);
+
+	returnValue = (funcList->C_Encrypt)(hSession, inBuffer, plainTextLength, outBuffer, &encrypted);
+	if(returnValue != CKR_OK) 
+	{
+			setError(FAILED_TO_ENCRYPT);
+			return "";
+	}
+	
+	string output = (char*)outBuffer;
+	return output;
+}
+
+bool encryptFile( string fileToEncrypt, string encryptedFile, string strKeyLabel )
+{
+	int retval = 0;
+	char buffer[BUFFER_SIZE];
+	ifstream fileRead;
+	ofstream fileWrite;
+	string strToEncrypt = "BOB";
+	string strEncrypted;
+	
+	fileRead.exceptions ( ifstream::eofbit | ifstream::failbit | ifstream::badbit );
+	fileWrite.exceptions ( ofstream::eofbit | ofstream::failbit | ofstream::badbit );
+	
+	try 
+	{
+		fileRead.open( fileToEncrypt.c_str() );
+        	while(fileRead.readsome(buffer, BUFFER_SIZE)) 
+			{  	 				
+            		strToEncrypt += buffer;
+        	}
+
+        	fileRead.close();
+	}
+	catch (ifstream::failure e) 
+	{
+		cout << "Exception opening/reading file";
+		setError(FAILED_TO_OPEN_READ_FILE);
+		return false;
+	}
+	
+	/* encrypt sucka */
+	strEncrypted = encrypt(strToEncrypt, strKeyLabel);
+
+	if(strEncrypted == "") {
+		return false;
+	}
+
+	/* writing header, data, footer to file */
+	try
+	{
+		/* create file */
+		fileWrite.open( encryptedFile.c_str(), fstream::in | fstream::out | fstream::app );
+		
+		/* write stream to file, APPEND MODE */
+		//fileWrite << "<header></header>";
+		fileWrite << "<type>encrypted</type>";
+		fileWrite << "<cert>" << strKeyLabel.c_str() << "</cert>";
+		fileWrite << "<enc>" << strEncrypted.c_str() << "</enc>";
+		//fileWrite << "footer></footer>";
+		
+		/* close */
+		fileWrite.close();
+	}
+	catch (ofstream:: failure e) 
+	{
+		cout << "Exception opening/writing to file";
+		setError(FAILED_TO_OPEN_WRITE_FILE);
+		return false;
+	}
+
+	return true;
+}
+
+void listCerts(void);
 
 int main(){
 	if(initCrypto()){
@@ -148,6 +268,141 @@ int main(){
 	else{
 		printf("Initialization error: %d", getLastError());
 	}
+	encryptFile( "bob.txt", "bob.enc", "bob" );
+	finalizeCrypto();
 	getchar();
 	return 0;
+}
+
+bool getPublicKey(string keyName, CK_OBJECT_HANDLE &pubKey) {
+	CK_OBJECT_CLASS pubKeyClass  = CKO_PUBLIC_KEY;
+	CK_KEY_TYPE KeyType = CKK_RSA;
+	CK_ULONG count;
+
+	CK_ATTRIBUTE pubKeyTemplate[] = {
+		{CKA_CLASS,		&pubKeyClass,	sizeof(CK_OBJECT_CLASS)},
+		{CKA_KEY_TYPE,	&KeyType,		sizeof(CK_KEY_TYPE)},
+		{CKA_TOKEN,		&True,			sizeof (True) },
+	};
+
+	returnValue = (funcList->C_FindObjectsInit)(hSession, pubKeyTemplate, 3);
+	if (returnValue != CKR_OK) {
+		return 0;
+	}
+
+	returnValue = (funcList->C_FindObjects)(hSession, &pubKey, 1, &count);		
+	if (returnValue != CKR_OK) {
+		return 0;
+	}
+	
+	returnValue = (funcList->C_FindObjectsFinal)(hSession);
+	if (returnValue != CKR_OK) {
+		return 0;
+	}
+
+	if (count < 1 ) {
+		setError(KEY_NOT_FOUND);
+		return 0;
+	}
+
+	return true;
+}
+
+void listCerts(void) {
+	CK_RV	rv;
+	CK_BBOOL bTrue = TRUE;		    
+	CK_BBOOL bFalse = FALSE;
+	CK_OBJECT_HANDLE hPubKey, hPrivKey;
+	
+	CK_ULONG ulObjectCount;			
+	CK_OBJECT_CLASS pubKeyClass  = CKO_PUBLIC_KEY;
+	CK_OBJECT_CLASS privKeyClass = CKO_PRIVATE_KEY;					
+	CK_KEY_TYPE KeyType = CKK_RSA;
+	CK_BYTE *tempKeyID;
+	CK_UTF8CHAR *tempLabel;
+
+	// public key search template 
+	CK_ATTRIBUTE pubKeyTemplate[] = {
+		{CKA_CLASS,		&pubKeyClass,	sizeof(CK_OBJECT_CLASS)},
+		{CKA_KEY_TYPE,	&KeyType,		sizeof(CK_KEY_TYPE)},
+		{CKA_TOKEN,		&bTrue,			sizeof (bTrue) },
+	};
+	
+	CK_ATTRIBUTE idTemplate[] = {
+		{CKA_ID,		NULL,			0},		
+	};
+
+	// private key search template 
+	CK_ATTRIBUTE privKeyTemplate[] = {
+		{CKA_CLASS,		&privKeyClass,	sizeof(CK_OBJECT_CLASS)},
+		{CKA_KEY_TYPE,	&KeyType,		sizeof(CK_KEY_TYPE)},
+		{CKA_TOKEN,		&bTrue,			sizeof (bTrue) },
+		{CKA_ID,		NULL,			0 },
+	};
+
+	CK_ATTRIBUTE labelTemplate[] = {
+		{CKA_LABEL,		NULL,			0},		
+	};
+
+	//1. Initialize public key search 
+		rv = (funcList->C_FindObjectsInit)(hSession, pubKeyTemplate, 3);
+		if (rv != CKR_OK) return;
+
+
+	//2. Search for public key
+		rv = (funcList->C_FindObjects)(hSession, &hPubKey, 2, &ulObjectCount);		
+		if (rv != CKR_OK) return;
+		
+		rv = (funcList->C_FindObjectsFinal)(hSession);
+		if (rv != CKR_OK) return;
+
+		if (ulObjectCount < 1 )
+		{
+			cout << "No public key found.";		
+			return;
+		} else
+			cout << "PUBLIC KEY FOUND.";	
+	
+
+	//3. Get CKA_ID of public key to match with corresponding private key
+		rv = (funcList->C_GetAttributeValue)(hSession, hPubKey, idTemplate, 1);
+		if (rv != CKR_OK) return;
+
+		tempKeyID = (CK_BYTE*)malloc(sizeof(CK_BYTE)*idTemplate[0].ulValueLen);
+		idTemplate[0].pValue = tempKeyID;
+
+		cout << endl;
+		for(int i = 0; i < idTemplate[0].ulValueLen; i++) {
+			cout << (int)tempKeyID[i];
+		}
+		cout << endl;
+
+		rv = (funcList->C_GetAttributeValue)(hSession, hPubKey, idTemplate, 1);
+		if (rv != CKR_OK) return;
+
+
+	//4. Initialize private key search 
+		privKeyTemplate[3].pValue = tempKeyID;
+		privKeyTemplate[3].ulValueLen = idTemplate[0].ulValueLen;
+
+		rv = (funcList->C_FindObjectsInit)(hSession, privKeyTemplate, 4);
+		if (rv != CKR_OK) return;
+
+
+	//5. Search for private key
+		ulObjectCount = 0;
+
+		rv = (funcList->C_FindObjects)(hSession, &hPrivKey, 1, &ulObjectCount);		
+		if (rv != CKR_OK) return;
+		
+		rv = (funcList->C_FindObjectsFinal)(hSession);
+		if (rv != CKR_OK) return;
+
+		if (ulObjectCount < 1 )
+		{
+			cout << "No private key found.";		
+			return;
+		} else
+			cout << "PRIVATE KEY FOUND.";	
+
 }
